ba3c746f2a64995570b53d60f56b132ba02833e5
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-05 17:41:12 +0430

    Add Routes

--- Routes/contactRoutes.js
+++ Routes/contactRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.route.get("/", (req, res) => {
-    res.json({"message":"Get All Contacts!"});
-})
+router.route("/").get((req, res) => {
+    res.status(200).json({"message":"Get All Contacts!"});
+});
 
 module.exports = router;
