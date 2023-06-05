fd869eda20ef929f334e76e7a62132e6f4d53a9b
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-05 16:39:00 +0430

    Add changes to server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.get('/api/contacts', (req, res) => {
+    res.send("Get All Contacts!");
+})
 
 app.listen(port, ()=> {
     console.log(`Server is running on ${port}`);
