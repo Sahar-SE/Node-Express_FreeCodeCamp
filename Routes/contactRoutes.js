a20fa67c1083dfb3728404b4dccba4e967ab006c
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-06 18:52:10 +0430

    Add contactController

--- Routes/contactRoutes.js
+++ Routes/contactRoutes.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getContact, createContact, updateContact, deleteContact, showContact } = require("../controllers/contactController");
+const { getContact, createContact, updateContact, deleteContact, showContact, } = require("../controllers/contactController");
 
-router.route("/").get(getContact);
-
-router.route("/").post(createContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
-
-router.route("/:id").get(showContact);
-
+router.route("/").get(getContact).post(createContact);
+router.route("/:id").put(updateContact).delete(deleteContact).get(showContact);
 
 module.exports = router;
