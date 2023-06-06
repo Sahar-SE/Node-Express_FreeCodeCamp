81e5728ca4a078f22094b8cfceae9bf7ebe4433d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-06 17:25:18 +0430

    Add contactController

--- Routes/contactRoutes.js
+++ Routes/contactRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { getContact } = require("../controllers/contactController")
 
 router.route("/").get((req, res) => {
     res.status(200).json({"message":"Get All Contacts!"});
