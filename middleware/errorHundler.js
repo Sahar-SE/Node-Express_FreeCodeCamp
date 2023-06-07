6567f9ccdbaec5479aca590ec31dffa45c62aeb6
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-07 23:24:46 +0430

    Add Middleware

--- middleware/errorHundler.js
+++ middleware/errorHundler.js
@@ -6,7 +6,7 @@ const errorHundler = (err, req, res, next) => {
       case constants.VALIDATION_ERROR:
         res.json({title: "Validation Faild", message: err.message, stackTrace: err.stack});
       break;
-      case 404:
+      case constants.NOT_FOUND:
         res.json({title: "Not Found", message: err.message, stackTrace: err.stack});
       break;   
         default:
