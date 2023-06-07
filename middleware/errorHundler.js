8df4c3c1e0219b8fb3d4220a23d947f0b9b88b82
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-07 23:23:13 +0430

    Add Middleware

--- middleware/errorHundler.js
+++ middleware/errorHundler.js
@@ -3,7 +3,7 @@ const { constants } = require("../constants");
 const errorHundler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
     switch (statusCode) {
-      case constants:
+      case constants.VALIDATION_ERROR:
         res.json({title: "Validation Faild", message: err.message, stackTrace: err.stack});
       break;
       case 404:
