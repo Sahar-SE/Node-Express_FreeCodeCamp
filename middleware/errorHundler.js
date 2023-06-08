32604e9109989781c21aa48743c7bc43babdcd7d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-08 17:37:23 +0430

    Add Middleware

--- middleware/errorHundler.js
+++ middleware/errorHundler.js
@@ -16,7 +16,7 @@ const errorHundler = (err, req, res, next) => {
         res.json({title: "Forbidden", message: err.message, stackTrace: err.stack});
       break;
       case constants.SERVER_ERROR:
-        res.json({title: "", message: err.message, stackTrace: err.stack});
+        res.json({title: "Server Error", message: err.message, stackTrace: err.stack});
       break;
         default:
             break;
