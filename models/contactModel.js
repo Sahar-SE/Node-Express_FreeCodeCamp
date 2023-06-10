2121c1bb9f9cbe2d375f79260f26b8f1a490e4a3
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-10 19:51:51 +0430

    Add db connection

--- models/contactModel.js
+++ models/contactModel.js
@@ -7,11 +7,13 @@ const contactSchema = mongoose.Schema({
   },
   email: {
     type: String,
-    required: [true, "Please add the contact email"]
+    required: [true, "Please add the email address"]
   },
   phone: {
     type: String,
-    
+    required: [true, "Please add the phone number"],
   }
 
 });
+
+module.exports = contactSchema;
