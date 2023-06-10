c79ffdfded5eec5c7b5f601b2d4bb1202d1b4ad6
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-10 18:41:03 +0430

    Add db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const  connectDb = async () => {
   try {
-
+    const connect = await mongoose.connect();
   }catch(err) {
     console.log(err);
     process.exit(1);
