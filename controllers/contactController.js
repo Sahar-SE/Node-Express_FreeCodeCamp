e906eeb93d13cf83039abf669eaa5c952309c563
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-13 18:02:34 +0430

    Add db connection

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -15,7 +15,7 @@ const createContact = asyncHandler(async(req, res) => {
   await console.log("The Request boby is:", req.body);
   const {name, email, phone} = req.boby;
   await console.log("2nd time", name);
-  if(!name || !lastname || !phone) {
+  if(!name || !email || !phone) {
     await res.status(400);
     throw new Error("Fields are empty");
   };
